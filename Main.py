e4e2198a716a1e94cbd6fff9aaacdd1b1b2a98a3
Jonathan W <jwilliams12117@gmail.com>
2023-01-23 21:30:40 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+"""This game will generate random stock conditions and prompt the user to buy or sell.  The goal
+of the game is to accumulate the highest score possible by timing purchases and sales.
+"""
+
+# ---------- Pseudocode ----------
+
+
+
+# ---------- Main Code ----------
