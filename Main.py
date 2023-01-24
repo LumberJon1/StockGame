e68eebd9019504b6722b28847d06a4b0b0caae95
Jonathan W <jwilliams12117@gmail.com>
2023-01-23 21:35:40 -0700

    Added pseudocode of structure to program

--- Main.py
+++ Main.py
@@ -2,8 +2,26 @@
 of the game is to accumulate the highest score possible by timing purchases and sales.
 """
 
+import random
+
 # ---------- Pseudocode ----------
 
+# Randomly generate a set of stocks and their prices
+
+# Set initial value of user's cash on hand
+
+# Prompt user to buy, sell, hold, or inspect their holdings
+
+# If the user selects buy, prompt them for the stock to buy and for how much
+
+# Validate inputs for existing stock tickers and to limit upper thresholds not to exceed balance
+
+# ELSE IF user selects sell, validate for existing holdings and show them, then prompt which to sell
+
+# Validate for a nonzero quantity and add the balance of proceeds to their account
+
+# Repeat loop
 
 
 # ---------- Main Code ----------
+
